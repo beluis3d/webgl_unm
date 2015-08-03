777646adb1a29c1e1a0441759b6f3cfff1a02c2c
Luis Bermudez <bermudez.luis3d@gmail.com>
2015-08-03 11:28:52 -0700

    added wireframes, fixed render syncing

--- Homework_003/script.js
+++ Homework_003/script.js
@@ -5,6 +5,10 @@ var curves = [];
 var points = [];
 var topPoints = [];
 var botPoints = [];
+var wireCurves = [];
+var wirePoints = [];
+var topWirePoints = [];
+var botWirePoints = [];
 createSpherePoints();
 var affine = mat4();
 
@@ -17,6 +21,7 @@ var scaleVec = vec3(1.0, 1.0, 1.0);
 
 var si = { // shader inputs
 	vBufferId: undefined,
+	wBufferId: undefined,
 	a_Location: undefined,
 	a_Affine: mat4()
 }
@@ -31,14 +36,13 @@ window.onload = function init() {
 	var canvas = document.getElementById("gl-canvas");
 	gl = WebGLUtils.setupWebGL(canvas);
 	gl.program = initShadersFromSource(gl, VSHADER_SOURCE, FSHADER_SOURCE);
-	gl.program2 = initShadersFromSource(gl, VSHADER_SOURCE, FSHADER_SOURCE2);
+	gl.program2 = initShadersFromSource(gl, VSHADER_SOURCE2, FSHADER_SOURCE2);
 	gl.useProgram(gl.program);
 
 	setupGUI();
 	setupData();
 	update();
-	render();
-
+	requestAnimFrame(render);
 }
 
 function setupGUI() {
@@ -55,28 +59,25 @@ function setupGUI() {
 	};
 
 	var gui = new dat.GUI();
-	gui.add( effectController, 'newTranslateX', 0.0, 1.0).step(0.1).name('TranslateX').onChange(function(value) {
+	gui.add( effectController, 'newTranslateX', -1.0, 1.0).step(0.1).name('TranslateX').onChange(function(value) {
 		if (effectController.newTranslateX !== translateVec[0]) {
 			translateVec[0] = effectController.newTranslateX;
 			translateSphere(translateVec[0], translateVec[1], translateVec[2]);
 			update();
-			render();
 		}
 	});
-	gui.add( effectController, 'newTranslateY', 0.0, 1.0).step(0.1).name('TranslateY').onChange(function(value) {
+	gui.add( effectController, 'newTranslateY', -1.0, 1.0).step(0.1).name('TranslateY').onChange(function(value) {
 		if (effectController.newTranslateY !== translateVec[1]) {
 			translateVec[1] = effectController.newTranslateY;
 			translateSphere(translateVec[0], translateVec[1], translateVec[2]);
 			update();
-			render();
 		}
 	});
-	gui.add( effectController, 'newTranslateZ', 0.0, 1.0).step(0.1).name('TranslateZ').onChange(function(value) {
+	gui.add( effectController, 'newTranslateZ', -1.0, 1.0).step(0.1).name('TranslateZ').onChange(function(value) {
 		if (effectController.newTranslateZ !== translateVec[2]) {
 			translateVec[2] = effectController.newTranslateZ;
 			translateSphere(translateVec[0], translateVec[1], translateVec[2]);
 			update();
-			render();
 		}
 	});
 	gui.add( effectController, 'newRotateX', 0.0, 360.0).step(1.0).name('RotateX').onChange(function(value) {
@@ -84,7 +85,6 @@ function setupGUI() {
 			rotateVec[0] = effectController.newRotateX;
 			rotateSphere(rotateVec[0], rotateVec[1], rotateVec[2]);
 			update();
-			render();
 		}
 	});
 	gui.add( effectController, 'newRotateY', 0.0, 360.0).step(1.0).name('RotateY').onChange(function(value) {
@@ -92,7 +92,6 @@ function setupGUI() {
 			rotateVec[1] = effectController.newRotateY;
 			rotateSphere(rotateVec[0], rotateVec[1], rotateVec[2]);
 			update();
-			render();
 		}
 	});
 	gui.add( effectController, 'newRotateZ', 0.0, 360.0).step(1.0).name('RotateZ').onChange(function(value) {
@@ -100,7 +99,6 @@ function setupGUI() {
 			rotateVec[2] = effectController.newRotateZ;
 			rotateSphere(rotateVec[0], rotateVec[1], rotateVec[2]);
 			update();
-			render();
 		}
 	});
 
@@ -109,7 +107,6 @@ function setupGUI() {
 			scaleVec[0] = effectController.newScaleX;
 			scaleSphere(scaleVec[0], scaleVec[1], scaleVec[2]);
 			update();
-			render();
 		}
 	});
 	gui.add( effectController, 'newScaleY', 0.0, 2.0).step(0.1).name('ScaleY').onChange(function(value) {
@@ -117,7 +114,6 @@ function setupGUI() {
 			scaleVec[1] = effectController.newScaleY;
 			scaleSphere(scaleVec[0], scaleVec[1], scaleVec[2]);
 			update();
-			render();
 		}
 	});
 	gui.add( effectController, 'newScaleZ', 0.0, 2.0).step(0.1).name('ScaleZ').onChange(function(value) {
@@ -125,15 +121,16 @@ function setupGUI() {
 			scaleVec[2] = effectController.newScaleZ;
 			scaleSphere(scaleVec[0], scaleVec[1], scaleVec[2]);
 			update();
-			render();
 		}
 	});
 }
 
 function setupData() {
 	gl.clearColor(0.0, 0.0, 0.0, 1.0);
+	gl.enable(gl.DEPTH_TEST);
 
 	si.vBufferId = gl.createBuffer();
+	si.wBufferId = gl.createBuffer();
 	gl.bindBuffer(gl.ARRAY_BUFFER, si.vBufferId);	
 	si.a_Location = gl.getAttribLocation(gl.program, "a_Location");
 	gl.vertexAttribPointer(si.a_Location, 3, gl.FLOAT, false, 0, 0);
@@ -145,7 +142,10 @@ function setupData() {
 function update() {
 	updateSphereAffineMatrix();
 
-	gl.bufferData(gl.ARRAY_BUFFER, flatten(points.concat(topPts).concat(botPts)), gl.STATIC_DRAW);
+	gl.bindBuffer(gl.ARRAY_BUFFER, si.vBufferId);
+	gl.bufferData(gl.ARRAY_BUFFER, flatten(points.concat(botPoints).concat(topPoints)), gl.STATIC_DRAW);
+	gl.bindBuffer(gl.ARRAY_BUFFER, si.wBufferId);
+	gl.bufferData(gl.ARRAY_BUFFER, flatten(wirePoints.concat(botWirePoints).concat(topWirePoints)), gl.STATIC_DRAW);
 	gl.vertexAttrib4f( si.a_Affine+0, affine[0][0], affine[1][0], affine[2][0], affine[3][0] );
 	gl.vertexAttrib4f( si.a_Affine+1, affine[0][1], affine[1][1], affine[2][1], affine[3][1] );
 	gl.vertexAttrib4f( si.a_Affine+2, affine[0][2], affine[1][2], affine[2][2], affine[3][2] );
@@ -155,17 +155,25 @@ function update() {
 function render() {
 	gl.clear(gl.COLOR_BUFFER_BIT);
 	gl.useProgram(gl.program);
+	gl.bindBuffer(gl.ARRAY_BUFFER, si.vBufferId);
+	gl.vertexAttribPointer(si.a_Location, 3, gl.FLOAT, false, 0, 0);
 	for (var i = 0; i < curves.length; i++) {
 		gl.drawArrays(gl.TRIANGLE_STRIP, curves[i].start, curves[i].size);	
 	}
-	gl.drawArrays(gl.TRIANGLE_FAN, points.length, topPts.length);
-	gl.drawArrays(gl.TRIANGLE_FAN, points.length+topPts.length, botPts.length);
+	gl.drawArrays(gl.TRIANGLE_FAN, points.length, botPoints.length);
+	gl.drawArrays(gl.TRIANGLE_FAN, points.length+botPoints.length, topPoints.length);
 
 
 	gl.useProgram(gl.program2);
-	for (var i = 0; i < curves.length; i++) {
-		gl.drawArrays(gl.LINE_STRIP, curves[i].start, curves[i].size);	
+	gl.bindBuffer(gl.ARRAY_BUFFER, si.wBufferId);
+	gl.vertexAttribPointer(si.a_Location, 3, gl.FLOAT, false, 0, 0);
+	for (var i = 0; i < wireCurves.length; i++) {
+		gl.drawArrays(gl.LINE_STRIP, wireCurves[i].start, wireCurves[i].size);	
 	}
+	gl.drawArrays(gl.LINE_STRIP, wirePoints.length, botWirePoints.length);
+	gl.drawArrays(gl.LINE_STRIP, wirePoints.length+botWirePoints.length, topWirePoints.length);
+
+	requestAnimFrame(render);
 }
 
 function translateSphere(x, y, z) { 
@@ -190,6 +198,7 @@ function createSpherePoints() {
 	var dk = 180.0/steps;
 	var radius = 1.0;
 
+	// TRIANGLE POINTS
 	for (var k = 0+dk; k < 180.0-dk; k+=dk) {
 		var kCurve = { start: points.length, size: 0 };
 		for (var i = 0.0; i <= 360.0; i+=di) {
@@ -201,18 +210,44 @@ function createSpherePoints() {
 		}
 		curves.push( kCurve );
 	}
-
-	var botPt = polarToCartesian(radius,0.0,0.0);
-	botPts = [botPt];
+	var botPoint = polarToCartesian(radius,0.0,0.0);
+	botPoints = [botPoint];
 	for(var i = 0; i <= 360.0; i+=di) {
 		var p1 = polarToCartesian(radius,i,0.0+dk);
-		botPts.push(p1);
+		botPoints.push(p1);
 	}
-	var topPt = polarToCartesian(radius,360.0,0.0);
-	topPts = [topPt];
+	var topPoint = polarToCartesian(radius,0.0,180.0);
+	topPoints = [topPoint];
 	for(var i = 0; i <= 360.0; i+=di) {
 		var p1 = polarToCartesian(radius,i,180.0-dk);
-		topPts.push(p1);
+		topPoints.push(p1);
+	}
+
+	//WIRE POINTS
+	for (var k = 0+dk; k < 180.0-dk; k+=dk) {
+		var kWireCurve = { start: wirePoints.length, size: 0 };
+		for (var i = 0.0; i <= 360.0; i+=2*di) {
+			var p1 = polarToCartesian(radius,i,k);
+			var p2 = polarToCartesian(radius,i,k+dk);
+			var p3 = polarToCartesian(radius,i+di,k+dk);
+			var p4 = polarToCartesian(radius,i+di,k);
+			
+			kWireCurve.size+=4;
+			wirePoints.push( p1, p2, p3, p4 );
+		}
+		wireCurves.push( kWireCurve );
+	}
+	var botWirePoint = polarToCartesian(radius,0.0,0.0);
+	for(var i = 0; i <= 360.0; i+=2*di) {
+		var p1 = polarToCartesian(radius,i,0.0+dk);
+		var p2 = polarToCartesian(radius,i+di,0.0+dk);
+		botWirePoints.push( botWirePoint, p1, p2 );
+	}
+	var topWirePoint = polarToCartesian(radius,0.0,180.0);
+	for(var i = 0; i <= 360.0; i+=2*di) {
+		var p1 = polarToCartesian(radius,i-di,180.0-dk); // this is the opposite of bottom (i-di)
+		var p2 = polarToCartesian(radius,i,180.0-dk);
+		topWirePoints.push( topWirePoint, p1, p2 );
 	}
 }
 
--- Homework_003/shaders.js
+++ Homework_003/shaders.js
@@ -11,6 +11,18 @@ var VSHADER_SOURCE =
     "}\n" +
     "";
 
+var VSHADER_SOURCE2 =
+    "// vertex.shader\n" +
+    "attribute vec4 a_Location;\n" +
+    "attribute mat4 a_Affine;\n" +
+    "varying vec4 v_Position;\n" +
+    "void main() {\n" +
+    "   gl_Position = a_Affine * a_Location;\n" +
+    "   gl_PointSize = 15.0;\n" +
+    "   v_Position = vec4(gl_Position.xyz, 1.0);//vec4(abs(gl_Position.x), abs(gl_Position.y), abs(gl_Position.z), 1.0);\n" +
+    "}\n" +
+    "";
+
 var FSHADER_SOURCE =
     "// fragment.shader\n" +
     "precision mediump float;\n" +
